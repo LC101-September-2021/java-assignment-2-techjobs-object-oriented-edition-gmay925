ef17758c109eeda5d8e48383782ac216f27b404d
Gia <gmay925@yahoo.com>
2022-02-10 12:59:25 -0500

    FinalUpdate

--- src/main/java/org/launchcode/techjobs/oo/Main.java
+++ src/main/java/org/launchcode/techjobs/oo/Main.java
@@ -10,17 +10,17 @@ public class Main {
 //        System.out.println(job1.toString());
 //        System.out.println(job1.getEmployer().getValue());
 //        System.out.println(job1.getId() + " " + job1.getEmployer().getId());
-//        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-//        Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
-//
-//        ArrayList<Job> jobs = new ArrayList<>();
-//        jobs.add(job1);
-//        jobs.add(job2);
-//        jobs.add(job3);
-//
-//        for (Job job : jobs){
-//            System.out.println(job);
-//        }
+        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+        Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+
+        ArrayList<Job> jobs = new ArrayList<>();
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+
+        for (Job job : jobs){
+            System.out.println(job);
+        }
     }
 
 }
